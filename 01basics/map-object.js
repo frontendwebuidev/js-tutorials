6a08008f07f319d5875373c592e276206ab48ead
Anil Maurya <43470655+frontendwebuidev@users.noreply.github.com>
2024-10-23 08:40:32 +0000

    map object

---
+++ 01basics/map-object.js
@@ -0,0 +1,15 @@
+let map = new Map();
+
+map.set('name', 'Anil Maurya');
+map.set('add', 10+10);
+map.set('data', {name:'anil', age:40, profession:'software developer'});
+
+
+console.log(map.get('data'));
+console.log(map.size);
+console.log(map.has('add'));
+console.log(map.delete('name'));
+console.log(map);
+
+//console.log(map.size);
+
